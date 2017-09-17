ddb05223015b6e166533418eb64fc9502cb7c797
Min Min <minmhan@gmail.com>
2017-09-17 13:07:11 +0800

    add main features

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'A test', 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg'),
+    new Recipe('A test recipe', 'A test', 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+}
