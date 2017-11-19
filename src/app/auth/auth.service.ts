fd27d2123c652c032c2ef6c1023252c2ebcdece9
Min Min <minmhan@gmail.com>
2017-11-19 21:29:22 +0800

    add modules

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService{
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
                 response => { 
-                    this.router.navigate(['/']);
+                    this.router.navigate(['/'])
                     firebase.auth().currentUser.getToken()
                         .then(
                             (token: string) => this.token = token
@@ -39,7 +39,7 @@ export class AuthService{
         return this.token;
     }
 
-    isAuthenticated(){
+    public isAuthenticated(){
         return this.token != null;
         //console.log(this.token);
     }
